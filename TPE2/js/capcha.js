432666d4d9a5be15b6ad43dfb99bd106a0bc23d3
Gustavo Arias <ariasgustavo3a@gmail.com>
2023-06-07 18:35:12 -0300

    nuevo repo para TPE2

---
+++ TPE2/js/capcha.js
@@ -0,0 +1,215 @@
+"use strict";
+
+let form = document.querySelector('#form'); //busca el boton en el DOM
+let capch = document.querySelector('#cap'); //busca en el DOM el titulo donde mostrara el nro aleatorio
+let boton = document.querySelector('#btn');
+let reset = document.querySelector('#reset'); //boton de reset
+
+//---------------------NUEVOS ALEATORIOS-------------
+let comp = "";
+captcha();
+
+
+function captcha() {
+  const CANTCARACTCAPTCH = 5;
+  for (let i = 0; i < CANTCARACTCAPTCH; i++) {
+    comp = comp + opcRandom();
+  }
+  capch.innerHTML = comp;
+}
+
+
+function opcRandom() {
+  let opc, cadena;
+  for (let i = 0; i<3; i++){
+    opc = Math.floor(Math.random()*3+1);
+    cadena = caracterAleatorio(opc);
+  }
+  return cadena;
+}
+
+
+function caracterAleatorio(opc) {
+  let caracter;
+  switch (opc) {
+    case 1: //digitos aleatorios
+      caracter = String.fromCharCode(Math.floor((Math.random()*(57-48)+1)+48));
+      break;
+    case 2: //letras mayusculas aleatorias
+      caracter = String.fromCharCode(Math.floor((Math.random()*(90-65)+1)+65));
+      break;
+    case 3: //letras minusculas aleatorias
+      caracter = String.fromCharCode(Math.floor((Math.random()*(122-97)+1)+97));
+      break;
+  };
+  return caracter;
+};
+
+
+form.addEventListener("submit",verificarcap); //cuando hay click en boton de enviar apunta a la funcion comparar
+reset.addEventListener("click",resetearCapcha);
+
+function verificarcap(e){ //funcion de verificacion y comparacion
+  e.preventDefault();
+  let verif = document.querySelector('#verif'); //busca en el DOM la casilla donde el usuario ingresará el nro para verificar
+  verif.classList.remove('vaciocapcha');
+  boton.classList.remove('vaciocapcha');
+  if ((comp === verif.value)){
+    boton.value=("CORRECTO!!");
+    verif.classList.add('greencapcha');
+    boton.classList.add('greencapcha');
+    let formularioRegHidden = document.querySelector('#registro_section').classList.toggle('ocultaRegistro');
+    let sectionShop = document.querySelector('#section_shop').classList.toggle('muestraRegistro');
+    document.querySelector('#saludo_usuario').innerHTML = `Bienvenid@ ${document.querySelector('#name_user').value}`;
+  }else{
+    boton.value=("INCORRECTO!!");
+    verif.classList.add('redcapcha');
+    boton.classList.add('redcapcha');
+  }
+};
+
+function resetearCapcha() {
+  let verif = document.querySelector('#verif');
+  boton.value=("Enviar");
+  verif.classList.remove('redcapcha');
+  boton.classList.remove('redcapcha');
+  verif.classList.add('vaciocapcha');
+  boton.classList.add('vaciocapcha');
+  comp = "";
+  capch.innerHTML = comp;
+  verif.value = comp;
+  captcha();
+};
+
+//----------------------------------------------------
+
+//----------------esaparecer el formulario
+//----------------y aparecer el shop
+
+
+
+
+
+
+/*
+*----------INICIO DE LA TABLA DINAMICA----------
+*/
+
+
+  let array_precios = [           /* precios de los elementos que vendo */
+  {
+    item: "Entrada 2D",
+    precioUnit: 1000,
+  },
+  {
+    item: "Entrada 3D",
+    precioUnit: 1400,
+  },
+  {
+    item: "Balde Pochoclos",
+    precioUnit: 1400,
+  },
+  {
+    item: "Combo Dúo",
+    precioUnit: 2600,
+  },
+  {
+    item: "Combo Familiar",
+    precioUnit: 4600,
+  },
+  {
+    item: "Combo Pelicula",
+    precioUnit: 3400,
+  },
+  {
+    item: "Garrapiñada",
+    precioUnit: 300,
+  },
+  {
+    item: "Milka Oreo",
+    precioUnit: 500,
+  },
+  {
+    item: "Shot",
+    precioUnit: 650,
+  },
+  {
+    item: "Vaso Gaseosa",
+    precioUnit: 500,
+  },
+];
+
+let cuerpoTablaShop = document.querySelector('#body_table_shop');
+let botonShopPush = document.querySelector('#agrega1');
+let inputCant = document.querySelector('#cant_items');
+let totalTabla = document.querySelector('#total_suma');
+const CANTMINDESCUENTO = 3;
+const DESCUENTOPORCENT = 0.2;
+let array_pedido=[];
+botonShopPush.addEventListener("click", agregaPedido);
+
+let sumaTotal = 0;
+
+function agregaPedido(e) {
+  e.preventDefault();
+  console.log("estoy dentro de consola boton");
+
+  let itemPedido = document.querySelector("#item").value;;
+  let cantidadItems = document.querySelector('#cant_items').value;
+  if (cantidadItems!="") {
+    let filaShop ={
+      item: itemPedido,
+      cantidad: parseInt(cantidadItems),
+      descuento: function () {
+        if(this.cantidad >= CANTMINDESCUENTO){
+          return true;
+        };
+      },
+      subTot: function () {
+        let position = 0;
+        while ((position < array_precios.length)&&(this.item != array_precios[position].item)) {
+          position++;
+        }
+        if (filaShop.descuento()){
+          return ((cantidadItems * (1-DESCUENTOPORCENT) * (array_precios[position].precioUnit)).toFixed(2));
+        } else {
+        return (cantidadItems * (array_precios[position].precioUnit));
+        }
+      },
+    };
+    array_pedido.push(filaShop);
+    let claseFila;
+    if (filaShop.descuento()) {
+      claseFila = "resaltaFila";
+    } else {
+      claseFila = "";
+    }
+    cuerpoTablaShop.innerHTML += `<tr class="${claseFila}"><td>${filaShop.item}</td><td>${filaShop.cantidad}</td><td>${filaShop.subTot()}</td></tr>`;
+    inputCant.value= "";
+    totalTabla.innerHTML = sumaTotal+=parseInt(filaShop.subTot());
+  }
+};
+
+let botonShopPop = document.querySelector('#borraUlt').addEventListener("click", ()=>{
+  sumaTotal-=array_pedido[array_pedido.length-1].subTot();
+  array_pedido.pop();
+  cuerpoTablaShop.innerHTML = "";
+  mostrarTabla();
+  totalTabla.innerHTML = sumaTotal;
+});
+
+let botonShopDel = document.querySelector('#eliminaTodo').addEventListener("click", () => {
+  cuerpoTablaShop.innerHTML = "";
+  totalTabla.innerHTML = "";
+});
+
+function mostrarTabla() {
+  for (let i = 0; i < array_pedido.length; i++) {
+    cuerpoTablaShop.innerHTML += `<tr><td>${array_pedido[i].item}</td><td>${array_pedido[i].cantidad}</td><td>${array_pedido[i].subTot()}</td></tr>`;
+  };
+}
+
+
+/*
+*----------FIN DE LA TABLA DINAMICA----------
+*/
---
+++ TPE2/js/slide.js
@@ -0,0 +1,54 @@
+"use strict"
+
+/*
+! ---- comienzo del slide --------------------
+*/
+
+let arregloImg = ["img0.png","img1.png","img2.png","img3.png","img4.png","img5.png","img6.png","img7.png","img8.png","img9.png"];
+let imgActual = 0;
+
+//setear tiempo
+let tempo;
+tempo = setInterval(pasarImagen, 5000);
+
+document.querySelector('#btn-sig').addEventListener("click" , pasarImagen);
+document.querySelector('#btn-ant').addEventListener("click" , pasarImagen);
+let contImgSlide = document.querySelector('#img-slide');
+
+contImgSlide.addEventListener("mouseover", detenerSlide);
+contImgSlide.addEventListener("mouseout", arrancarSlide);
+
+function pasarImagen() {
+  let boton= this.id;
+  if (boton!=undefined) {
+    detenerSlide();
+    arrancarSlide();
+  }
+  if (boton=='btn-sig') {
+    imgActual++;
+    if(imgActual==arregloImg.length){
+      imgActual = 0;
+    }
+  } else {
+      imgActual--;
+      if (imgActual<0) {
+        imgActual=arregloImg.length-1;
+      }
+    }
+  let showImage = arregloImg[imgActual];
+  //console.log(imgActual);
+  //console.log(showImage);
+  contImgSlide.style = "background-image: url(img/"+showImage+");transition: 0.5s;";
+}
+
+function detenerSlide() {
+  clearInterval(tempo);
+}
+
+function arrancarSlide() {
+  tempo = setInterval(pasarImagen, 5000);
+}
+
+/*
+! ---------- FIN del slide ------------------------------
+*/

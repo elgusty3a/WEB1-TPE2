4e5e6460d944bb0b46a769b384c460badef05c1c
Gustavo Arias <ariasgustavo3a@gmail.com>
2023-06-07 22:27:36 -0300

    nuevo banner descuento

--- TPE2/js/slide.js
+++ TPE2/js/slide.js
@@ -4,7 +4,7 @@
 ! ---- comienzo del slide --------------------
 */
 
-let arregloImg = ["img0.png","img1.png","img2.png","img3.png","img4.png","img5.png","img6.png","img7.png","img8.png","img9.png"];
+let arregloImg = ["img0.png","img1.png","img2.png","img3.png","img4.png","img5.png","img6.png","img7.png","img8.png","img9.png","img10.png"];
 let imgActual = 0;
 
 //setear tiempo
